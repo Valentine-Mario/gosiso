d7f1c21d48b49293194b104c6a8fcd87ff1d3a34
valentine-mario <valentine13400@gmail.com>
2020-06-12 12:20:13 +0100

    finished payment integration

---
+++ helpers/payment.js
@@ -0,0 +1,24 @@
+let PayStack = require('paystack-node')
+require('dotenv').config()
+let APIKEY = process.env.APIKEY_PAY
+const environment = process.env.NODE_ENV
+ 
+const paystack = new PayStack(APIKEY, environment)
+ 
+
+
+   
+  class Paystack_class{
+   verifyTransaction(ref){
+    return new Promise((resolve, reject)=>{
+        paystack.verifyTransaction({
+            reference:ref
+           }).then(res=>{
+               resolve(res)
+           }).catch(err=>{
+               reject(errr)
+           })
+    })
+}
+  }
+  module.exports=new Paystack_class()
