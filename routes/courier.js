9bacfc07e6a21afd1eb95d5a898ac0143fbc5898
valentine-mario <valentine13400@gmail.com>
2020-09-14 16:23:56 +0100

    bug fix

--- routes/courier.js
+++ routes/courier.js
@@ -5,7 +5,7 @@ const courierController=require('../controller/courier')
 
 router.post('/apply', verification.verifyToken, courierController.applyAsCourier)
 router.get('/get/:id', courierController.getCourierById)
-router.get('/search/:value', courierController.searchCourier)
+router.get('/search/:value', verification.verifyToken, courierController.searchCourier)
 router.post('/rate/:id', verification.verifyToken, courierController.rateCourier)
 router.get('/getallstate', courierController.getAllcourierStates)
 router.post('/setavailablestate', verification.verifyToken, courierController.setAvailableState)
